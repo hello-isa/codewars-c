f23f72a4d114c8870b1a3587ae7e58b3ea8bba33
hello-isa <isabeljoyadriatico@gmail.com>
2023-12-06 15:38:32 +0800

    Add new problem

---
+++ coefficient-quadratic-solver.c
@@ -0,0 +1,51 @@
+/*
+In this Kata you are expected to find the coefficients of quadratic equation of the given two roots (x1 and x2).
+
+Equation will be the form of ax^2 + bx + c = 0
+
+Return type is a Vector (tuple in Rust, Array in Ruby) containing coefficients of the equations in the order (a, b, c).
+
+Since there are infinitely many solutions to this problem, we fix a = 1.
+
+Remember, the roots can be written like (x-x1) * (x-x2) = 0
+
+Example
+quadratic(1,2) = (1, -3, 2)
+This means (x-1) * (x-2) = 0; when we do the multiplication this becomes x^2 - 3x + 2 = 0
+
+Example 2
+quadratic(0,1) = (1, -1, 0)
+This means (x-0) * (x-1) = 0; when we do the multiplication this becomes x^2 - x + 0 = 0
+
+Notes
+Inputs will be integers.
+When x1 == x2, this means the root has the multiplicity of two
+*/
+
+#include <stdio.h>
+
+void quadratic(int x1, int x2, int coefficients[3]);
+
+int main()
+{
+    int ce[3];
+
+    quadratic(1, 2, ce);
+    quadratic(0, 1, ce);
+
+    return 0;
+}
+
+void quadratic(int x1, int x2, int coefficients[3])
+{
+    coefficients[0] = 1;             // First term
+    coefficients[1] = -(x2) + -(x1); // Second term
+    coefficients[2] = -(x1) * -(x2); // Third term
+
+    int n;
+    for (n = 0; n < 3; n++)
+    {
+        printf("%d ", coefficients[n]);
+    }
+    printf("\n");
+}
